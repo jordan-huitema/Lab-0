d23348d73e436f31523fe6c1c72f451ce96a2b8a
Jordan <jordan-huitema@live.nmit.ac.nz>
2020-03-02 15:01:08 +1300

    Monday commit

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("Hello world");
+
+/*--setting varibals--*/
+var firstName;
+var lastName;
+var numOne;
+var numTwo;
+var sum;
+var sub;
+var div;
+
+/*--asigning varibals--*/
+firstName = "Jordan";
+lastName = "Huitema";
+numOne = 5;
+numTwo = 6;
+sum = (numOne+numTwo);
+sub = (numOne-numTwo);
+div = (numOne/numTwo);
+//console.log(lastName);
+//console.log(firstName);
+
+/*--calling varibals--*/
+console.log(firstName +" "+ lastName);
+console.log("number 1 = "+ numOne);
+console.log("number 2 = "+ numTwo);
+console.log("sum = "+ sum);
+console.log("sub = "+ sub);
+console.log("div = "+ div);
